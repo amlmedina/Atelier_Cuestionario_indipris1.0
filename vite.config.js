5c63688b5b5bb88f0c101ce17e94ebdce45be994
amlmedina <43222292+amlmedina@users.noreply.github.com>
2026-08-17 22:44:34 -0600

    Actualizar URLs a 1.0.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Atelier_Cuestionario_indipris/',
+  base: '/Atelier_Cuestionario_indipris1.0/',
 })
